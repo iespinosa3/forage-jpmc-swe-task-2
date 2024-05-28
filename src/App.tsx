e4954f5471102d129ca677892d6c7bea7b41def6
iespinosa <espi_nosa3@yahoo.com>
2024-05-28 14:23:20 -0400

    updated ticket requirements

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
-  showGraph: boolean,
+  showGraph:boolean,
 }
 
 /**
@@ -23,7 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
-      showGraph: false,
+      showGraph:false,
     };
   }
 
@@ -33,31 +33,28 @@ class App extends Component<{}, IState> {
   renderGraph() {
     if(this.state.showGraph){
     return (<Graph data={this.state.data}/>)
-     }
+    }
   }
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    let x= 0;
-    const interval =setInterval(() =>{
-      DataStreamer.getData((serverResponds: ServerRespond[]) => {
-        // Update the state by creating a new array of data that consists of
-        // Previous data in the state and the new data from server
-        this.setState ({ 
-          data:serverResponds,
-          showGraph: true, 
-         });
+    let x =0;
+    const interval =setInterval(() => {
+    DataStreamer.getData((serverResponds: ServerRespond[]) => {
+      // Update the state by creating a new array of data that consists of
+      // Previous data in the state and the new data from server
+      this.setState({ 
+        data: serverResponds,
+        showGraph:true
 
-        });
-        x++;
-        if (x > 1000){
-          clearInterval(interval);
-        }
-      
-      }, 100);
-
-
+      });
+    });
+    x++
+    if(x > 1000){
+      clearInterval(interval);
+       }
+    },100)
   }
 
   /**
